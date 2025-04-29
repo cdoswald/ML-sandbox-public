f847548660ee3c9ebfe0b9bf185ce511f8d37990
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2025-04-29 10:11:33 -0400

    added functions to load and filter parquet files

--- 3D_semantic_seg/src/utils/utils.py
+++ 3D_semantic_seg/src/utils/utils.py
@@ -1,5 +1,7 @@
 """General utility functions for Waymo Open Dataset challenges."""
 
+from functools import reduce
+import operator
 import os
 from typing import Dict, List, Optional, Union, Tuple
 import warnings
@@ -7,6 +9,8 @@ import warnings
 import matplotlib.pyplot as plt
 import pyarrow
 import pyarrow.compute as pc
+import pyarrow.dataset as pds
+import pyarrow.parquet as pq
 import tensorflow
 import tensorflow.compat.v1 as tf
 
@@ -15,13 +19,76 @@ tf.enable_eager_execution()
 from waymo_open_dataset import dataset_pb2 as wod
 
 
-def filter_table(
+def get_parquet_col_names(
+    data_dir: str,
+    file_id: str,
+    data_subdir: Optional[str] = "",
+) -> List[str]:
+    """Get column names of parquet file without loading into memory.
+
+    Args
+        data_dir: data directory name
+        file_id: file name (excluding .parquet suffix)
+        data_subdir: optional data subdirectory name
+    """
+    if not isinstance(data_subdir, str):
+        raise ValueError(
+            f"data_subdir must be empty or non-empty str, but got type {type(data_subdir)}"
+        )
+    file_path = os.path.join(data_dir, data_subdir, f"{file_id}.parquet")
+    return pds.dataset(file_path).schema.names
+
+def load_parquet_data(
+    data_dir: str,
+    file_id: str,
+    data_subdir: Optional[str] = "",
+    subset_cols: Optional[List[str]] = None,
+    filter_rows: Optional[Dict[str, Union[str, int, float]]] = None,
+) -> pyarrow.Table:
+    """Load parquet dataset.
+
+    Args
+        data_dir: data directory name
+        file_id: file name (excluding .parquet suffix)
+        data_subdir: optional data subdirectory name
+        subset_cols: optional list of column names to retain
+        filter_rows: optional dict of {col_name:filter_val} to filter rows
+
+    Returns
+        pyarrow Table with filtered rows and columns (if applicable)
+    """
+    if not isinstance(data_subdir, str):
+        raise ValueError(
+            f"data_subdir must be empty or non-empty str, but got type {type(data_subdir)}"
+        )
+    file_path = os.path.join(data_dir, data_subdir, f"{file_id}.parquet")
+    data = pds.dataset(file_path, format="parquet")
+    # Filter rows and columns
+    row_filter = None
+    if filter_rows is not None:
+        filter_exprs = [(pds.field(col) == val) for col, val in filter_rows.items()]
+        row_filter = reduce(operator.and_, expressions)
+    if subset_cols is None:
+        subset_cols = data.schema.names
+    return data.to_table(filter=row_filter, columns=subset_cols)
+
+def filter_table_equal(
     table: pyarrow.lib.Table,
-    filter_col: str,
-    filter_val: Union[str, int],
+    filter_dict: Dict[str, Union[str, int, float]],
 ) -> pyarrow.lib.Table:
-    """Filter pyarrow table where 'filter_col' matches 'filter_val'."""
-    mask = pc.equal(table[filter_col], filter_val)
+    """Filter pyarrow table using equality matches.
+
+    Args
+        table: pyarrow table to filter
+        filter_dict: mapping of {col_name:filter_val}
+
+    Returns
+        filtered pyarrow table
+    """
+    mask = pyarrow.array([True] * len(table))
+    for col_name, filter_val in filter_dict.items():
+        condition_match = pc.equal(table[col_name], filter_val)
+        mask = pc.and_kleene(mask, condition_match)
     return table.filter(mask)
 
 def load_TFRecord(

d752764afe66dde6ecb3101893a6f653c26aab24
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2025-05-31 09:23:55 -0400

    added semseg class visualization

--- 3D_semantic_seg/src/visualize_pointcloud.py
+++ 3D_semantic_seg/src/visualize_pointcloud.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import open3d as o3d
 
 POINTCLOUD_PATH = "../pointcloud.npz"
@@ -8,15 +9,31 @@ CAM_ZOOM = 0.2
 data = np.load(POINTCLOUD_PATH)
 points = data["points"]
 
-# Separate (x,y,z) and semseg labels (if applicable)
-points_xyz = points[..., :3].reshape((-1, 3))
-if points.shape[-1] > 3:
-    points_semseg = points[..., 3:]
-
 # Create Open3D visualization
 pcd = o3d.geometry.PointCloud()
+
+# Add (x,y,z) coords
+points_xyz = points[..., :3].reshape((-1, 3))
 pcd.points = o3d.utility.Vector3dVector(points_xyz)
 
+# Add semseg labels (if applicable)
+if points.shape[-1] > 3:
+    points_semseg = points[..., 3:]
+    points_instances = points_semseg[..., 0].flatten()
+    points_classes = points_semseg[..., 1].flatten()
+
+    # Map semseg classes to colormap
+    cmap = plt.get_cmap("tab20")
+    unique_classes = np.unique(points_classes)
+    n_unique_classes = len(unique_classes)
+    color_dict = {
+        label: cmap(i/n_unique_classes)[:3]
+        for i, label in enumerate(unique_classes)
+    }
+    colors = np.array([color_dict[label] for label in points_classes])
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+
+# Adjust default camera position
 cam_lookat = np.array([0, 0, 0])
 cam_up = np.array([0, 0, 1])
 cam_front = np.array([-1, 0, 1])

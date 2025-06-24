357038e232efc2a85803d15fff71e1ebb37da1bf
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2025-06-23 23:07:55 -0400

    updated to handle sequences of LiDAR pointclouds

--- 3D_semantic_seg/src/visualize_pointcloud.py
+++ 3D_semantic_seg/src/visualize_pointcloud.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,19 +9,41 @@ from utils import utils_constants as utl_c
 
 POINTCLOUD_DIR = "pointclouds"
 CAM_ZOOM = 0.2
+FRAME_DELAY_SEC = 0.2
 
 # Get list of pointcloud files
-pointcloud_files = [x for x in os.listdir(POINTCLOUD_DIR) if x.endswith(".npz")]
+pointcloud_files = [
+    x for x in os.listdir(POINTCLOUD_DIR) if x.endswith(".npz")
+]
 
+# Create Open3D visualizer
+vis = o3d.visualization.Visualizer()
+vis.create_window()
+vis.add_geometry(o3d.geometry.TriangleMesh.create_coordinate_frame())
+
+# Adjust default camera position
+cam_lookat = np.array([0, 0, 0])
+cam_up = np.array([0, 0, 1])
+cam_front = np.array([-1, 0, 1])
+cam_front = cam_front / np.linalg.norm(cam_front)
+
+ctr = vis.get_view_control()
+ctr.set_lookat(cam_lookat)
+ctr.set_front(cam_front)
+ctr.set_up(cam_up)
+ctr.set_zoom(CAM_ZOOM)
+
+# Create geometry object
+pcd = o3d.geometry.PointCloud()
+
+# Display data stream
+first_pass = True
 for pointcloud_file in pointcloud_files:
 
     # Load and extract data
     data = np.load(os.path.join(POINTCLOUD_DIR, pointcloud_file))
 
     for obs_id, points in data.items():
-
-        # Create Open3D visualization
-        pcd = o3d.geometry.PointCloud()
 
         # Add (x,y,z) coords
         points_xyz = points[..., :3].reshape((-1, 3))
@@ -37,28 +60,18 @@ for pointcloud_file in pointcloud_files:
             colors = np.array([color_dict[label] for label in points_classes])
             pcd.colors = o3d.utility.Vector3dVector(colors)
 
-        # Adjust default camera position
-        cam_lookat = np.array([0, 0, 0])
-        cam_up = np.array([0, 0, 1])
-        cam_front = np.array([-1, 0, 1])
-        cam_front = cam_front / np.linalg.norm(cam_front)
+        # Update geometry
+        if first_pass:
+            vis.add_geometry(pcd)
+        else:
+            vis.update_geometry(pcd)
 
-        vis = o3d.visualization.Visualizer()
-        vis.create_window()
-        vis.add_geometry(pcd)
-        vis.add_geometry(
-            o3d.geometry.TriangleMesh.create_coordinate_frame()
-        )
+        vis.poll_events()
+        vis.update_renderer()
+        time.sleep(FRAME_DELAY_SEC)
 
-        ctr = vis.get_view_control()
-        ctr.set_lookat(cam_lookat)
-        ctr.set_front(cam_front)
-        ctr.set_up(cam_up)
-        ctr.set_zoom(CAM_ZOOM)
+# Clean up
+vis.destroy_window()
 
-        vis.run()
-
-        vis.destroy_window()
-
-        break #TODO: remove
-    break #TODO: remove
+#TODO: left off here 6/23 @ 11pm; need to adjust camera view for LiDAR stream
+#TODO: record frames and output as video using OpenCV

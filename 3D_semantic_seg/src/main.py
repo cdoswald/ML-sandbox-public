dd36fff0a2941867046d0ca302dd165bd738c19a
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2025-06-29 23:12:49 -0400

    updated main

--- 3D_semantic_seg/src/main.py
+++ 3D_semantic_seg/src/main.py
@@ -1,5 +1,11 @@
 """Main execution"""
 
+interactive_mode = True #TODO: remove
+if interactive_mode:
+    import os
+    os.chdir("/workspace/hostfiles/src")
+    print(os.getcwd())
+
 import io
 import logging
 import os
@@ -14,14 +20,19 @@ import polars as pl
 import torch
 
 from waymo_open_dataset import dataset_pb2 as wod
-from waymo_open_dataset.utils import frame_utils
+from waymo_open_dataset.utils import (
+    frame_utils,
+    range_image_utils,
+)
 
 from models import PlaceholderModel
 
-from utils import utils as utl
-from utils import utils_constants as utl_c
-from utils import utils_plotting as utl_p
-from utils import utils_waymo as utl_w
+from utils import utils as util
+from utils import utils_camera as util_cam
+from utils import utils_constants as util_cons
+from utils import utils_lidar as util_li
+from utils import utils_plotting as util_p
+from utils import utils_waymo as util_w
 
 
 if __name__ == "__main__":
@@ -32,13 +43,18 @@ if __name__ == "__main__":
     # logger.debug("Logger set-up successful")
 
     # Define constants
-    LASER_NAME_MAP = dict(wod.LaserName.Name.items())
-    CAMERA_NAME_MAP = dict(wod.CameraName.Name.items())
-    LIDAR_RETURN_MAP = dict()
-    RANGE_IMAGE_DIM_MAP = utl_c.get_range_image_final_dim_dict()
-    SEG_IMAGE_DIM_MAP = utl_c.get_seg_image_final_dim_dict()
+    # LASER_NAME_MAP = dict(wod.LaserName.Name.items())
+    # CAMERA_NAME_MAP = dict(wod.CameraName.Name.items())
+    # LIDAR_RETURN_MAP = dict()
+    # RANGE_IMAGE_DIM_MAP = util_cons.get_range_image_final_dim_dict()
+    # SEG_IMAGE_DIM_MAP = util_cons.get_seg_image_final_dim_dict()
 
-    DATA_DIR = "/workspace/hostfiles/3DSemanticSeg/data"
+    DATA_DIR = "/workspace/hostfiles/data"
+    VIDEOS_DIR = "/workspace/hostfiles/videos"
+    POINTCLOUD_DIR = "/workspace/hostfiles/pointclouds"
+
+    os.makedirs(VIDEOS_DIR, exist_ok=True)
+    os.makedirs(POINTCLOUD_DIR, exist_ok=True)
     
     # Get list of file IDs that are present in all data subfolders
     file_ids = []
@@ -59,147 +75,171 @@ if __name__ == "__main__":
 
     # Load data for file ID
     for file_id in file_ids:
-        file_id = file_ids[0] #TODO: remove
 
-        camera_image_table = pq.read_table(
-            os.path.join(DATA_DIR, "camera_image", f"{file_id}.parquet"),
+        # Get column names of tables
+        camera_image_cols = util.get_parquet_col_names(DATA_DIR, file_id, "camera_image")
+        camera_box_cols = util.get_parquet_col_names(DATA_DIR, file_id, "camera_box")
+        camera_calib_cols = util.get_parquet_col_names(DATA_DIR, file_id, "camera_calibration")
+        camera_segment_cols = util.get_parquet_col_names(DATA_DIR, file_id, "camera_segmentation")
+
+        lidar_image_cols = util.get_parquet_col_names(DATA_DIR, file_id, "lidar")
+        lidar_calib_cols = util.get_parquet_col_names(DATA_DIR, file_id, "lidar_calibration")
+        lidar_segment_cols = util.get_parquet_col_names(DATA_DIR, file_id, "lidar_segmentation")
+
+        # Get list of observation ids that have 3D semantic segmentation labels
+        lidar_segment_table_all = util.load_parquet_data(DATA_DIR, file_id, "lidar_segmentation")
+        labeled_obs_ids = lidar_segment_table_all["index"].combine_chunks().to_pylist()
+        print(f"# of labeled obs ids: {len(labeled_obs_ids)}")
+
+        # Extract labeled LiDAR data and corresponding camera images
+        filter_lidar_rows = {"index":labeled_obs_ids, "key.laser_name":[1]}
+        filter_camera_rows = {"index":labeled_obs_ids, "key.camera_name":list(np.arange(1, 6))}
+
+        camera_image_table = util.load_parquet_data(
+            DATA_DIR, file_id, "camera_image", filter_rows=filter_camera_rows
         )
-        camera_box_table = pq.read_table(
-            os.path.join(DATA_DIR, "camera_box", f"{file_id}.parquet")
+        camera_box_table = util.load_parquet_data(
+            DATA_DIR, file_id, "camera_box", filter_rows=filter_camera_rows
         )
-        camera_calib_table = pq.read_table(
-            os.path.join(DATA_DIR, "camera_calibration", f"{file_id}.parquet")
+        camera_calib_table = util.load_parquet_data(
+            DATA_DIR, file_id, "camera_calibration", filter_rows=filter_camera_rows
         )
-        camera_segment_table = pq.read_table(
-            os.path.join(DATA_DIR, "camera_segmentation", f"{file_id}.parquet")
+        camera_segment_table = util.load_parquet_data(
+            DATA_DIR, file_id, "camera_segmentation", filter_rows=filter_camera_rows
         )
 
-        # Get list of observation IDs in camera image file
-        obs_ids = [str(x) for x in np.unique(camera_image_table["index"].to_pylist())]
+        lidar_calib_table = util.load_parquet_data(
+            DATA_DIR, file_id, "lidar_calibration", filter_rows=filter_lidar_rows
+        )
+        lidar_segment_table = util.load_parquet_data(
+            DATA_DIR, file_id, "lidar_segmentation", filter_rows=filter_lidar_rows
+        )
 
-        # Filter data for observation ID
-        for obs_id in obs_ids:
-            obs_id = obs_ids[0] #TODO: remove
-            break #TODO: remove
+        # Display camera images with object boxes
+        camera_image_table_obs = util.filter_rows_equal(camera_image_table, filter_camera_rows)
     
-            # Display camera images with object boxes
-            camera_ids = utl.filter_table(camera_image_table, "index", obs_id).column(
-                "key.camera_name"
-            )
-            for camera_id in camera_ids:
-                utl_p.show_camera_image_with_bboxes(
-                    camera_image_table,
+        camera_frames = {}
+        for camera_id in range(10):
+            print(f"Trying camera id {camera_id}")
+            try:
+                camera_frames[camera_id] = util_cam.extract_camera_images(
+                    camera_image_table_obs,
                     camera_box_table,
-                    obs_id,
                     camera_id=camera_id,
                 )
+            except ValueError as e:
+                print(f"Could not extract frames for camera_id={camera_id}; got error: '{e}'")
         
-            DEL = utl.filter_table(camera_segment_table, "index", obs_id)
-            DEL = utl.filter_table(DEL, "key.camera_name", 1)
+        # util_cam.display_frames(camera_frames) # Multicamera
+        # util_cam.display_frames(camera_frames[1]) # Single camera
         
+        # util_cam.write_frames_to_video_file(
+        #     camera_frames,
+        #     VIDEOS_DIR,
+        #     "test_2",
+        # )
+        
+        #TODO: left off here; need to create a sequence of LiDAR 3D point clouds
+            
+        # Get lidar range values for one observation at a time due to memory constraints
+        all_points_dict = {}
+        for labeled_obs_id in sorted(labeled_obs_ids):
+            filter_lidar_rows["index"] = [labeled_obs_id]
+            lidar_image_table = util.load_parquet_data(
+                DATA_DIR, file_id, "lidar", filter_rows=filter_lidar_rows
+            )
+            points = util_li.convert_lidar_range_image_to_xyz_coords(
+                lidar_image_table, lidar_calib_table, lidar_segment_table, convert_to_world_ref=False
+            )
+            all_points_dict[labeled_obs_id] = points
 
+        np.savez(
+            os.path.join(POINTCLOUD_DIR, f"pointcloud_{file_id}.npz"),
+            **{k:v for k,v in all_points_dict.items()}
+        )
 
+        break
 
-
-
-# Camera images
-['index',
- 'key.segment_context_name',
- 'key.frame_timestamp_micros',
- 'key.camera_name',
- '[CameraImageComponent].image',
- '[CameraImageComponent].pose.transform', # Transform from camera ref to vehicle ref system
- '[CameraImageComponent].velocity.linear_velocity.x',
- '[CameraImageComponent].velocity.linear_velocity.y',
- '[CameraImageComponent].velocity.linear_velocity.z',
- '[CameraImageComponent].velocity.angular_velocity.x',
- '[CameraImageComponent].velocity.angular_velocity.y',
- '[CameraImageComponent].velocity.angular_velocity.z',
- '[CameraImageComponent].pose_timestamp',
- '[CameraImageComponent].rolling_shutter_params.shutter',
- '[CameraImageComponent].rolling_shutter_params.camera_trigger_time',
- '[CameraImageComponent].rolling_shutter_params.camera_readout_done_time']
-
-# Camera boxes
-['index',
- 'key.segment_context_name',
- 'key.frame_timestamp_micros',
- 'key.camera_name',
- 'key.camera_object_id',
- '[CameraBoxComponent].box.center.x',
- '[CameraBoxComponent].box.center.y',
- '[CameraBoxComponent].box.size.x',
- '[CameraBoxComponent].box.size.y',
- '[CameraBoxComponent].type',
- '[CameraBoxComponent].difficulty_level.detection',
- '[CameraBoxComponent].difficulty_level.tracking']
-
-# Camera calibration
-['key.segment_context_name',
- 'key.camera_name',
- '[CameraCalibrationComponent].intrinsic.f_u',
- '[CameraCalibrationComponent].intrinsic.f_v',
- '[CameraCalibrationComponent].intrinsic.c_u',
- '[CameraCalibrationComponent].intrinsic.c_v',
- '[CameraCalibrationComponent].intrinsic.k1',
- '[CameraCalibrationComponent].intrinsic.k2',
- '[CameraCalibrationComponent].intrinsic.p1',
- '[CameraCalibrationComponent].intrinsic.p2',
- '[CameraCalibrationComponent].intrinsic.k3',
- '[CameraCalibrationComponent].extrinsic.transform',
- '[CameraCalibrationComponent].width',
- '[CameraCalibrationComponent].height',
- '[CameraCalibrationComponent].rolling_shutter_direction']
     
-    # Extract frames
-    frames = utl.extract_frames_from_datafile(dataset)
-    frame = frames[24]  # TODO: generalize
 
-    # Parse range images
-    range_images, camera_projections, seg_labels, range_image_top_pose = (
-        frame_utils.parse_range_image_and_camera_projection(frame)
-    )
+            
+            # TODO:
+            # - modify to handle batch > 1 observation
 
-    # Convert range and segmentation images to tensors
-    ## range_images, camera_projections, and seg_labels are
-    ## dictionaries formatted: {laser_index: [return1, return2]}
-    range_image_tensor = utl.convert_range_image_to_tensor(
-        range_images[LASER_NAME_MAP["TOP"]][0]
-    )
-    cp_tensor = utl.convert_range_image_to_tensor(
-        camera_projections[LASER_NAME_MAP["TOP"]][0]
-    )
-    seg_image_tensor = utl.convert_range_image_to_tensor(
-        seg_labels[LASER_NAME_MAP["TOP"]][0]
-    )
 
-    # Plot example range and segmentation image
-    utl_p.plot_range_image_tensor(
-        range_image_tensor,
-        RANGE_IMAGE_DIM_MAP,
-        invert_colormap=True,
-    )
-    utl_p.plot_range_image_tensor(
-        seg_image_tensor, SEG_IMAGE_DIM_MAP, style_params={"cmap": "tab20"}
-    )
+                
+        
+            
 
-    # Plot example point cloud
-    points, cp_points = frame_utils.convert_range_image_to_point_cloud(
-        frame,
-        range_images,
-        camera_projections,
-        range_image_top_pose,
-        ri_index=0,  # First return
-    )
-    point_labels = utl_w.convert_range_image_to_point_cloud_labels(
-        frame,
-        range_images,
-        seg_labels,
-        ri_index=0,  # First return
-    )
-    points_all = np.concatenate(points, axis=0)
-    point_labels_all = np.concatenate(point_labels, axis=0)
-    cp_points_all = np.concatenate(cp_points, axis=0)
+
+# "index" = key.segment_context_name + key.frame_timestamp_micros
+
+
+
+
+
+
+    
+
+
+        
+
+
+
+
+
+
+
+
+#     # Extract frames
+#     frames = util.extract_frames_from_datafile(dataset)
+#     frame = frames[24]  # TODO: generalize
+
+#     # Parse range images
+#     range_images, camera_projections, seg_labels, range_image_top_pose = (
+#         frame_utils.parse_range_image_and_camera_projection(frame)
+#     )
+
+#     # Convert range and segmentation images to tensors
+#     ## range_images, camera_projections, and seg_labels are
+#     ## dictionaries formatted: {laser_index: [return1, return2]}
+#     range_image_tensor = util.convert_range_image_to_tensor(
+#         range_images[LASER_NAME_MAP["TOP"]][0]
+#     )
+#     cp_tensor = util.convert_range_image_to_tensor(
+#         camera_projections[LASER_NAME_MAP["TOP"]][0]
+#     )
+#     seg_image_tensor = util.convert_range_image_to_tensor(
+#         seg_labels[LASER_NAME_MAP["TOP"]][0]
+#     )
+
+#     # Plot example range and segmentation image
+#     util_p.plot_range_image_tensor(
+#         range_image_tensor,
+#         RANGE_IMAGE_DIM_MAP,
+#         invert_colormap=True,
+#     )
+#     util_p.plot_range_image_tensor(
+#         seg_image_tensor, SEG_IMAGE_DIM_MAP, style_params={"cmap": "tab20"}
+#     )
+
+#     # Plot example point cloud
+#     points, cp_points = frame_utils.convert_range_image_to_point_cloud(
+#         frame,
+#         range_images,
+#         camera_projections,
+#         range_image_top_pose,
+#         ri_index=0,  # First return
+#     )
+#     point_labels = util_w.convert_range_image_to_point_cloud_labels(
+#         frame,
+#         range_images,
+#         seg_labels,
+#         ri_index=0,  # First return
+#     )
+#     points_all = np.concatenate(points, axis=0)
+#     point_labels_all = np.concatenate(point_labels, axis=0)
+#     cp_points_all = np.concatenate(cp_points, axis=0)
 
     # TODO: plot point cloud
     
@@ -217,16 +257,16 @@ if __name__ == "__main__":
     # ax = fig.add_subplot(111, projection='3d')
     # ax.scatter(cp_points_all[:, 3], cp_points_all[:, 4], cp_points_all[:, 5])
     
-    # Placeholder model (getting pipeline set up before iterating on model development)
-    model = PlaceholderModel()
-    range_image_tensor = torch.from_numpy(range_image_tensor.numpy())
-    output = model(range_image_tensor)
+    # # Placeholder model (getting pipeline set up before iterating on model development)
+    # model = PlaceholderModel()
+    # range_image_tensor = torch.from_numpy(range_image_tensor.numpy())
+    # output = model(range_image_tensor)
 
-    # Generate protobuff submission file for validation set
+    # # Generate protobuff submission file for validation set
 
 
-    range_image_tensor.shape
-    TOP_LIDAR_ROW_NUM = 64
-    TOP_LIDAR_COL_NUM = 2650
+    # range_image_tensor.shape
+    # TOP_LIDAR_ROW_NUM = 64
+    # TOP_LIDAR_COL_NUM = 2650
     
  

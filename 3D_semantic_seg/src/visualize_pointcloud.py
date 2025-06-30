3927b873521adb0da260938f940fcfb0f6eca12c
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2025-06-29 23:11:47 -0400

    added saving pointcloud to video

--- 3D_semantic_seg/src/visualize_pointcloud.py
+++ 3D_semantic_seg/src/visualize_pointcloud.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import open3d as o3d
@@ -8,29 +9,39 @@ import open3d as o3d
 from utils import utils_constants as utl_c
 
 POINTCLOUD_DIR = "pointclouds"
+VIDEOS_DIR = "videos"
+
 CAM_ZOOM = 0.2
 FRAME_DELAY_SEC = 0.2
+FRAME_SIZE = (1280, 720)
+FPS = int(1 / FRAME_DELAY_SEC)
 
 # Get list of pointcloud files
 pointcloud_files = [
     x for x in os.listdir(POINTCLOUD_DIR) if x.endswith(".npz")
 ]
 
-# Create Open3D visualizer
-vis = o3d.visualization.Visualizer()
-vis.create_window()
-vis.add_geometry(o3d.geometry.TriangleMesh.create_coordinate_frame())
-
-# Create geometry object
-pcd = o3d.geometry.PointCloud()
-
-# Display data stream
-first_pass = True
+# Create LiDAR pointcloud video for each pointcloud file
 for pointcloud_file in pointcloud_files:
+
+    # Create OpenCV video writer
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    video_file = os.path.join(VIDEOS_DIR, pointcloud_file.replace(".npz", ".mp4"))
+    video_writer = cv2.VideoWriter(video_file, fourcc, FPS, FRAME_SIZE)
+
+    # Create Open3D visualizer
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(width=FRAME_SIZE[0], height=FRAME_SIZE[1])
+    vis.add_geometry(o3d.geometry.TriangleMesh.create_coordinate_frame())
+
+    # Create geometry object
+    pcd = o3d.geometry.PointCloud()
 
     # Load and extract data
     data = np.load(os.path.join(POINTCLOUD_DIR, pointcloud_file))
 
+    # Display data stream
+    first_pass = True
     for obs_id, points in data.items():
 
         # Add (x,y,z) coords
@@ -51,6 +62,7 @@ for pointcloud_file in pointcloud_files:
         # Update geometry
         if first_pass:
             vis.add_geometry(pcd)
+            first_pass = False
         else:
             vis.update_geometry(pcd)
 
@@ -66,11 +78,23 @@ for pointcloud_file in pointcloud_files:
         ctr.set_up(cam_up)
         ctr.set_zoom(CAM_ZOOM)
 
+        # Render image
         vis.poll_events()
         vis.update_renderer()
+
+        # Capture frame and convert to BGR for OpenCV output
+        img = vis.capture_screen_float_buffer(do_render=False)
+        img = (np.asarray(img) * 255).astype(np.uint8)
+        img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        # Write frame to video
+        video_writer.write(img_bgr)
+
         time.sleep(FRAME_DELAY_SEC)
 
-# Clean up
-vis.destroy_window()
+    # Clean up
+    video_writer.release()
+    vis.destroy_window()
 
-#TODO: left off here 6/23 @ 11pm; output as video using OpenCV
+# TODO: left off here on 6/29 @ 11:10pm; second pointcloud video was blank; check 
+# underlying pointcloud data

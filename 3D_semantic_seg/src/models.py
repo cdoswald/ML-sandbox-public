5ad5e3f3116e11f415f076544675f302ec1b6431
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2025-11-16 22:06:46 -0500

    updated BaselineCNN

--- 3D_semantic_seg/src/models.py
+++ 3D_semantic_seg/src/models.py
@@ -4,11 +4,46 @@ import torch
 from torch import nn
 
 
-class PlaceholderModel(nn.Module):
-    """Doc string"""
+class BaselineCNN(nn.Module):
+    """Baseline Convolutional Neural Network (CNN) Model."""
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        example_batch: torch.Tensor,
+        hidden_layers: list[int],
+        out_channels: int,
+        conv_kernel_size: int = 5,
+        avgpool_kernel_size: int = 2,
+    ) -> None:
         super().__init__()
+        self.batch_size, self.in_channels, self.height, self.width = example_batch.shape
 
+        # Calculate padding needed to preserve spatial dims
+        pad_h = (self.height - 1) // 2
+        pad_w = (self.width - 1) // 2
+
+        # Add layers to ModuleList
+        self.layers = nn.ModuleList()
+        next_in_channels = self.in_channels
+        for hidden_channels in hidden_layers:
+            conv_layer = nn.Conv2d(
+                next_in_channels,
+                hidden_channels,
+                conv_kernel_size,
+                padding=(pad_h, pad_w),
+            )
+            layer_norm = nn.LayerNorm((hidden_channels, self.height, self.width)) # this might not be the optimal normalization for lidar range images?
+            nonlin_layer = nn.ReLU()
+            avgpool_layer = nn.AvgPool2d(avgpool_kernel_size)
+            self.layers.extend([conv_layer, nonlin_layer, avgpool_layer])
+            next_in_channels = hidden_channels
+        
+        # Add fully connected layers to ModuleList
+        
+        # Test forward pass/check spatial dims are positive
+        
+        
     def forward(self, x):
-        return torch.ones_like(x) * 14
+        for layer in self.layers:
+            x = layer(x)
+        return x

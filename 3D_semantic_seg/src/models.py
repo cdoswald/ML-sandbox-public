bf19e71f26b691d23b1e50981bda42d613c9a31c
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2025-11-20 22:38:41 -0500

    updated BaselineCNN model

--- 3D_semantic_seg/src/models.py
+++ 3D_semantic_seg/src/models.py
@@ -8,17 +8,17 @@ class BaselineCNN(nn.Module):
     """Baseline Convolutional Neural Network (CNN) model
     
     Characteristics:
-        - Image is padded to maintain spatial dimensions
-        - Downsampling using average pooling
-        - Normalization using layer norm
-        - ReLU activation function
+        - Image is padded to maintain spatial dimensions during convolutions
+        - Downsampling (height and width) using average pooling
+        - Normalization using 2-D instance norm (distribution for each obs, channel)
+        - SiLU activation function
     """
 
     def __init__(
         self,
         example_batch: torch.Tensor,
-        hidden_layers: list[int],
         out_channels: int,
+        hidden_layers: list[int],
         conv_kernel_size: int = 5,
         avgpool_kernel_size: int = 2,
     ) -> None:
@@ -35,7 +35,7 @@ class BaselineCNN(nn.Module):
         final_height = self.height / reduc_factor
         final_width = self.width / reduc_factor
         if final_height < 1 or final_width < 1:
-            raise ValueError("Too many hidden layers; spatial dims are non-existent by end of model")
+            raise ValueError("Too many hidden layers; spatial dims are non-existent after encoding")
 
         # Create encoder
         encoder_layers = nn.ModuleList()
@@ -43,16 +43,18 @@ class BaselineCNN(nn.Module):
         curr_height = self.height
         curr_width = self.width
         for hidden_channels in hidden_layers:
-            conv_layer = nn.Conv2d(
+            encode_conv_layer = nn.Conv2d(
                 curr_in_channels,
                 hidden_channels,
                 conv_kernel_size,
                 padding=padding,
             )
-            layer_norm = nn.LayerNorm((hidden_channels, curr_height, curr_width)) # this might not be optimal for lidar range images
-            nonlin_layer = nn.ReLU()
+            norm_layer = nn.InstanceNorm2d(hidden_channels)
+            nonlin_layer = nn.SiLU()
             avgpool_layer = nn.AvgPool2d(avgpool_kernel_size)
-            encoder_layers.extend([conv_layer, layer_norm, nonlin_layer, avgpool_layer])
+            encoder_layers.extend(
+                [encode_conv_layer, norm_layer, nonlin_layer, avgpool_layer]
+            )
             curr_in_channels = hidden_channels
             curr_height //= avgpool_kernel_size
             curr_width //= avgpool_kernel_size
@@ -60,13 +62,21 @@ class BaselineCNN(nn.Module):
         self.encoder = nn.Sequential(*encoder_layers)
         print(f"Encoder: {self.encoder}")
 
-        # Create decoder
-        flattened_dim = curr_in_channels * curr_height * curr_width
-        flatten_layer = nn.Flatten()
-        fc1 = nn.Linear(flattened_dim, 256)
-        fc2 = nn.Linear(256, out_channels)
+        # Create decoder (upsample to restore spatial dimension)
+        decoder_layers = nn.ModuleList()
+        for hidden_channels in reversed(hidden_layers[1:]):
+            upsample_layer = nn.Upsample(
+                scale_factor=avgpool_kernel_size,
+                mode="bilinear",
+                align_corners=False,
+            )
+            decode_conv_layer = nn.Conv2d(
+                # TODO: left off here at 10:40pm on 11/20; need to determine channels, height, width 
+                # for convolutional layers in decoding block
+            )
+            
         
-        self.decoder = nn.Sequential(flatten_layer, fc1, fc2)
+        self.decoder = nn.Sequential(*decoder_layers)
         print(f"Decoder: {self.decoder}")
 
 

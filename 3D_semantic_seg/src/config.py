89983329ec9d9e876ede27226fb478ae7aa39423
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2026-05-08 00:28:15 +0000

    updated config for GCP

--- 3D_semantic_seg/src/config.py
+++ 3D_semantic_seg/src/config.py
@@ -5,19 +5,26 @@ import os
 class Config:
     """Configuration parameters"""
 
-    data_dir = "/workspace/hostfiles/data"
-    videos_dir = "/workspace/hostfiles/videos"
-    pointcloud_dir = "/workspace/hostfiles/pointclouds"
+    use_gcp: bool = True
+    gcp_data_dir: str = "gs://waymo_open_data_copy"
+    gcp_project_name: str = "waymo-3d-semseg"
+
+    local_data_dir: str = "/workspace/hostfiles/data"
+    videos_dir: str = "/workspace/hostfiles/videos"
+    pointcloud_dir: str = "/workspace/hostfiles/pointclouds"
 
     max_epochs: int = 2
     batch_size: int = 4
     lr: float = 1e-3
-    
+
     validation_share: float = 0.2
     test_share: float = 0.2
 
     def __post_init__(self):
+        self.data_dir = self.gcp_data_dir if self.use_gcp else self.local_data_dir
 
-        # Create directories
+        # Create local directories only when running locally
+        if not self.use_gcp:
+            os.makedirs(self.data_dir, exist_ok=True)
         os.makedirs(self.videos_dir, exist_ok=True)
         os.makedirs(self.pointcloud_dir, exist_ok=True)

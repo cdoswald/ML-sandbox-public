56712de29dc0e89e2de2dab689c6642a8a705da2
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2026-05-15 02:25:13 +0000

    added scene visualization params

--- 3D_semantic_seg/src/config.py
+++ 3D_semantic_seg/src/config.py
@@ -13,12 +13,18 @@ class Config:
     videos_dir: str = "/workspace/hostfiles/videos"
     pointcloud_dir: str = "/workspace/hostfiles/pointclouds"
 
+    # Scene visualization
+    scene_vis_n_workers: int = 4
+    scene_vis_fps: float = 5.0
+    scene_vis_stitch_videos: bool = True
+
     max_epochs: int = 2
     batch_size: int = 4
     lr: float = 1e-3
 
     validation_share: float = 0.2
     test_share: float = 0.2
+
 
     def __post_init__(self):
         self.data_dir = self.gcp_data_dir if self.use_gcp else self.local_data_dir

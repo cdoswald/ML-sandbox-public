e573906338572e176b53c1b3155b18988c82fa59
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2026-07-31 21:38:00 -0400

    ruffed

--- model_predictive_control/mujoco/src/utils_video.py
+++ model_predictive_control/mujoco/src/utils_video.py
@@ -1,5 +1,5 @@
 import cv2
-import os
+
 
 def display_video_from_frame_list(frame_list, frame_delay=33):
     """
@@ -9,7 +9,7 @@ def display_video_from_frame_list(frame_list, frame_delay=33):
         frame_list (list): List of frames (images) to display.
         frame_delay (int): Delay between frames in milliseconds. Default is 33ms (~30 FPS).
     """
-    window_name = 'Video Playback'
+    window_name = "Video Playback"
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
     for frame in frame_list:
@@ -18,13 +18,13 @@ def display_video_from_frame_list(frame_list, frame_delay=33):
         else:
             cv2_color_conv_code = cv2.COLOR_RGB2BGR
         cv2.imshow(window_name, cv2.cvtColor(frame, cv2_color_conv_code))
-        
+
         # Exit on 'q' key press
-        if cv2.waitKey(frame_delay) & 0xFF == ord('q'):
+        if cv2.waitKey(frame_delay) & 0xFF == ord("q"):
             break
 
     while cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) >= 1:
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        if cv2.waitKey(100) & 0xFF == ord("q"):
             break
 
     cv2.destroyAllWindows()
